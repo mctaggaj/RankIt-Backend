f740a73b52b4f7752d1f3b12a78e22e2c811b438
Shawn Hind <hind.shawn@gmail.com>
2015-02-21 18:12:23 -0500

    Added a new python module to virtual environment and added classes for representing database data within python

---
+++ database.py
@@ -0,0 +1,82 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey
+
+engine = create_engine('mysql://compuser:smellslikesoup@131.104.49.60/comp', echo=True)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'User'
+    userId = Column(Integer, primary_key=True)
+    firstName = Column(String)
+    lastName = Column(String)
+    password = Column(String)
+    bio = Column(String)
+
+    def __repr__(self):
+        return "<User(firstName='%s', lastName='%s', password='%s', bio='%s')>" % (self.firstName,
+                self.lastName, self.password, self.bio)
+
+class Permission(Base):
+    __tablename__ = 'Permission'
+    permId = Column(Integer, primary_key=True)
+    view = Column(Integer)
+    update = Column(Integer)
+    score = Column(Integer)
+
+class Competition(Base):
+    __tablename__ = 'Competition'
+    competitionId = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    location = Column(String)
+    participants = Column(String)
+    streamUrl = Column(String)
+    subject = Column(String)
+    visibility = Column(Integer)
+    results = Column(String)
+
+    def __repr__(self):
+        return '''<Competition(competitionId='%s', name='%s', description='%s', location='%s', participants='%s', 
+            streamUrl='%s', subject='%s', visibility='%s', results='%s')>''' % (self.competitionId, self.name,
+                    self.location, self.participants, self.streamUrl, self.subject, self.visibility, self.results)
+
+class CompetitionRole(Base):
+    __tablename__ = 'CompetitionRole'
+    compRoleId = Column(Integer, primary_key=True)
+    compId = Column(Integer, ForeignKey('Competition.competitionId'))
+    userId = Column(Integer, ForeignKey('User.userId'))
+    permId = Column(Integer, ForeignKey('Permission.permId'))
+
+    competition = relationship("Competition", backref=backref('compRoles'))
+    user = relationship("User", backref=backref('compRoles'))
+    permission = relationship("Permission", backref=backref('compRoles'))
+
+
+def main():
+    ed_user = User(firstName="Edward", lastName="Paulson", password="smellslikesoup", bio="Hi my name is edward paulson")
+    competition = Competition(name="ChickenWing")
+    permission = Permission(view=0, update=0, score=0)
+    compRole = CompetitionRole()
+
+    session = Session()
+    session.add(ed_user)
+    session.add(permission)
+    session.add(competition)
+    session.add(compRole)
+
+    competition.compRoles.append(compRole)
+    permission.compRoles.append(compRole)
+    ed_user.compRoles.append(compRole)
+
+
+    session.commit()
+
+if __name__ == "__main__":
+    main()
